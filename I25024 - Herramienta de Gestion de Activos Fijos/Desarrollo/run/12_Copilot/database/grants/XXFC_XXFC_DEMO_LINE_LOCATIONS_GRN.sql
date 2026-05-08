3154a5b47c18bca72237376a98c79c41fcda0175
gilbertoantonioh <gilbertoantonioh+oxxogh@hexaware.com>
2026-05-08 15:44:33 -0600

    Fix file naming and folder structure for demo line locations artifacts

--- I25024 - Herramienta de Gestion de Activos Fijos/Desarrollo/run/12_Copilot/database/grants/XXFC_XXFC_DEMO_LINE_LOCATIONS_GRN.sql
+++
@@ -1,8 +0,0 @@
-SET DEFINE OFF;
-PROMPT GRANT PRIVILEGES ON xxfc.xxfc_demo_line_locations
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON xxfc.xxfc_demo_line_locations TO APPS;
-GRANT SELECT, INSERT, UPDATE, DELETE ON xxfc.xxfc_demo_line_locations TO APPSRO;
-
-PROMPT CREATE SYNONYM apps.xxfc_demo_line_locations
-CREATE OR REPLACE SYNONYM apps.xxfc_demo_line_locations FOR xxfc.xxfc_demo_line_locations;
