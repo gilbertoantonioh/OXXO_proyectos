0308984958468a0b28054a495db9eb6426240b17
gilbertoantonioh <gilbertoantonioh+oxxogh@hexaware.com>
2026-05-08 15:22:57 -0600

    Update files

---
+++ I25024 - Herramienta de Gestion de Activos Fijos/Desarrollo/run/12_Copilot/database/synonyms/APPSVIEW_XXFC_XXFC_DEMOS_SYN.sql
@@ -0,0 +1,3 @@
+SET DEFINE OFF;
+PROMPT CREATE SYNONYM appsview.xxfc_xxfc_demos
+CREATE OR REPLACE SYNONYM appsview.xxfc_xxfc_demos FOR apps.xxfc_xxfc_demos;
---
+++ I25024 - Herramienta de Gestion de Activos Fijos/Desarrollo/run/12_Copilot/database/synonyms/APPSVIEW_XXFC_XXFC_DEMO_LINES_SYN.sql
@@ -0,0 +1,3 @@
+SET DEFINE OFF;
+PROMPT CREATE SYNONYM appsview.xxfc_xxfc_demo_lines
+CREATE OR REPLACE SYNONYM appsview.xxfc_xxfc_demo_lines FOR apps.xxfc_xxfc_demo_lines;
